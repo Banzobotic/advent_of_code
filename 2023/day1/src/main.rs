dd17232d6dde82eb955bbbe5b8695b8e8ff7e7f5
Banzobotic <78510207+Banzobotic@users.noreply.github.com>
2023-12-01 08:26:09 +0000

    day1 new new solution

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -1,12 +1,12 @@
-use aho_corasick::AhoCorasick;
-
 fn searchf(s: &str) -> char {
     let num_pos = s.chars().position(|c| c.is_numeric()).unwrap_or(10000);
-    let word_pos = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-        .iter()
-        .map(|num| s.match_indices(num).next().unwrap_or((10000, "nope")))
-        .min_by_key(|num| num.0)
-        .unwrap();
+    let word_pos = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ]
+    .iter()
+    .map(|num| s.match_indices(num).next().unwrap_or((10000, "nope")))
+    .min_by_key(|num| num.0)
+    .unwrap();
 
     if num_pos < word_pos.0 {
         s.chars().find(|c| c.is_numeric()).unwrap()
@@ -31,11 +31,13 @@ fn searchf(s: &str) -> char {
 
 fn searchb(s: &str) -> char {
     let num_pos = s.len() - s.chars().rev().position(|c| c.is_numeric()).unwrap_or(0) - 1;
-    let word_pos = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-        .iter()
-        .map(|num| s.rmatch_indices(num).next().unwrap_or((0, "pain")))
-        .max_by_key(|num| num.0)
-        .unwrap();
+    let word_pos = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ]
+    .iter()
+    .map(|num| s.rmatch_indices(num).next().unwrap_or((0, "pain")))
+    .max_by_key(|num| num.0)
+    .unwrap();
 
     if num_pos >= word_pos.0 && s.chars().any(|c| c.is_numeric()) {
         s.chars().rev().find(|c| c.is_numeric()).unwrap()
@@ -80,13 +82,9 @@ fn b() {
         "{}",
         include_str!("input")
             .lines()
-            .map(|l| format!(
-                "{}{}",
-                searchf(l),
-                searchb(l)
-            )
-            .parse::<u32>()
-            .unwrap())
+            .map(|l| format!("{}{}", searchf(l), searchb(l))
+                .parse::<u32>()
+                .unwrap())
             .sum::<u32>()
     );
 }
@@ -96,18 +94,32 @@ fn b_new() {
         "{}",
         include_str!("input")
             .lines()
-            .map(|l| {
-                let search = AhoCorasick::new(["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]).unwrap();
-                let expanded = search.replace_all(l, &["oonee", "ttwoo", "tthreee", "ffourr", "ffivee", "ssixx", "ssevenn", "eeightt", "nninee"]);
-                search.replace_all(&expanded, &["1", "2", "3", "4", "5", "6", "7", "8", "9"])
-            })
-            .map(|l| format!(
-                "{}{}",
-                l.chars().find(|c| c.is_numeric()).unwrap(),
-                l.chars().rev().find(|c| c.is_numeric()).unwrap()
+            .map(|l| l
+                .chars()
+                .enumerate()
+                .filter_map(|(i, c)| if c.is_numeric() {
+                    Some(c)
+                } else {
+                    let trimmed: String = l.chars().skip(i).collect();
+
+                    for (j, pat) in [
+                        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+                    ]
+                    .iter()
+                    .enumerate()
+                    {
+                        if trimmed.starts_with(pat) {
+                            return Some((j as u8 + 49) as char);
+                        }
+                    }
+
+                    None
+                }))
+            .map(
+                |l| format!("{}{}", l.clone().next().unwrap(), l.last().unwrap())
+                    .parse::<u32>()
+                    .unwrap()
             )
-            .parse::<u32>()
-            .unwrap())
             .sum::<u32>()
     );
 }
