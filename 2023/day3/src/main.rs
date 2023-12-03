eb8796dcaeb7184f5002c2b61124b4457bf72f38
Banzobotic <78510207+Banzobotic@users.noreply.github.com>
2023-12-03 09:59:41 +0000

    New vastly better solutions

--- 2023/day3/src/main.rs
+++ 2023/day3/src/main.rs
@@ -1,18 +1,35 @@
+use itertools::Itertools;
+use regex::Regex;
+
 fn a() {
-    let input: Vec<Vec<_>> = include_str!("input").lines().map(|l| l.chars().collect()).collect();
+    let input: Vec<Vec<_>> = include_str!("input")
+        .lines()
+        .map(|l| l.chars().collect())
+        .collect();
     let mut sum = 0;
-    let adjacent_offsets = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)];
+    let adjacent_offsets = [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, 1),
+        (1, 1),
+        (1, 0),
+        (1, -1),
+        (0, -1),
+    ];
 
     for (row, line) in input.iter().enumerate() {
         let mut buffer = Vec::new();
         let mut symbol = false;
 
-        for (i, c) in line.iter().enumerate() {          
+        for (i, c) in line.iter().enumerate() {
             if c.is_numeric() {
                 for (row_offset, i_offset) in adjacent_offsets.iter() {
                     let row_check = row as i32 + row_offset;
                     let i_check = i as i32 + i_offset;
-                    if (0..input.len() as i32).contains(&row_check) && (0..line.len() as i32).contains(&i_check) {
+                    if (0..input.len() as i32).contains(&row_check)
+                        && (0..line.len() as i32).contains(&i_check)
+                    {
                         symbol = match input[row_check as usize][i_check as usize] {
                             '0'..='9' | '.' => symbol,
                             _ => true,
@@ -26,7 +43,7 @@ fn a() {
                     let num: String = buffer.clone().into_iter().collect();
                     sum += num.parse::<i32>().unwrap();
                 }
-                
+
                 buffer.clear();
                 symbol = false;
             }
@@ -42,9 +59,21 @@ fn a() {
 }
 
 fn b() {
-    let input: Vec<Vec<_>> = include_str!("input").lines().map(|l| l.chars().collect()).collect();
+    let input: Vec<Vec<_>> = include_str!("input")
+        .lines()
+        .map(|l| l.chars().collect())
+        .collect();
     let mut sum = 0;
-    let adjacent_offsets = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)];
+    let adjacent_offsets = [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, 1),
+        (1, 1),
+        (1, 0),
+        (1, -1),
+        (0, -1),
+    ];
 
     for (row, line) in input.iter().enumerate() {
         for (i, c) in line.iter().enumerate() {
@@ -58,7 +87,9 @@ fn b() {
             for (row_offset, i_offset) in adjacent_offsets.iter() {
                 let row_check = row as i32 + row_offset;
                 let i_check = i as i32 + i_offset;
-                if (0..input.len() as i32).contains(&row_check) && (0..line.len() as i32).contains(&i_check) {
+                if (0..input.len() as i32).contains(&row_check)
+                    && (0..line.len() as i32).contains(&i_check)
+                {
                     let c_check = input[row_check as usize][i_check as usize];
 
                     if (!contiguous || *row_offset == 0) && c_check.is_numeric() {
@@ -68,7 +99,7 @@ fn b() {
 
                         loop {
                             buffer.insert(0, digit);
-                            
+
                             offset -= 1;
                             digit = input[row_check as usize][(i_check + offset) as usize];
 
@@ -87,7 +118,7 @@ fn b() {
 
                         while digit.is_numeric() {
                             buffer.push(digit);
-                            
+
                             offset += 1;
                             digit = input[row_check as usize][(i_check + offset) as usize];
 
@@ -122,7 +153,54 @@ fn b() {
     println!("{}", sum);
 }
 
+fn a_new() {
+    let input = include_str!("input").lines().collect_vec();
+    let re = Regex::new(r"\d+").unwrap();
+
+    println!(
+        "{}",
+        input
+            .windows(3)
+            .flat_map(|lines| lines[1]
+                .char_indices()
+                .filter(|(_, c)| !matches!(c, '0'..='9' | '.'))
+                .map(|(i, _)| lines
+                    .iter()
+                    .flat_map(|l| re
+                        .find_iter(l)
+                        .filter(|m| i <= m.end() && m.start() <= i + 1)
+                        .map(|m| l[m.range()].parse::<u32>().unwrap()))
+                    .sum::<u32>()))
+            .sum::<u32>()
+    );
+}
+
+fn b_new() {
+    let input = include_str!("input").lines().collect_vec();
+    let re = Regex::new(r"\d+").unwrap();
+
+    println!(
+        "{}",
+        input
+            .windows(3)
+            .flat_map(|lines| lines[1]
+                .char_indices()
+                .filter(|(_, c)| *c == '*')
+                .map(|(i, _)| lines
+                    .iter()
+                    .flat_map(|l| re
+                        .find_iter(l)
+                        .filter(|m| i <= m.end() && m.start() <= i + 1)
+                        .map(|m| l[m.range()].parse::<u32>().unwrap()))
+                    .fold((0, 1), |(count, product), x| (count + 1, product * x))))
+            .filter(|(count, _)| *count == 2)
+            .fold(0, |sum, (_, x)| sum + x)
+    );
+}
+
 fn main() {
     a();
     b();
+    a_new();
+    b_new();
 }
